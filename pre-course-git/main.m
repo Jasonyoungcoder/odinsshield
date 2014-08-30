b9b4fed3bced631669d6f023b8db871f5ad8f795
Jason Young <jasonyoung@Bryans-MacBook.local>
2014-08-30 11:21:42 -0600

    pre-course-git material, first project

---
+++ pre-course-git/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pre-course-git
+//
+//  Created by Jason Young on 8/29/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
